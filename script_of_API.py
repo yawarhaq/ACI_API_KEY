27ca1c9ba6bd2caec6f275003f357d0e36149eff
Mohammad Yawar Haq <142677047+yawarhaq@users.noreply.github.com>
2024-08-29 16:26:42 +0530

    Update script_of_API.py

--- script_of_API.py
+++ script_of_API.py
@@ -6,8 +6,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # APIC details
 apic_url = "https://172.16.100.75"
-username = "vector"
-password = "Vector@123"
+username = "your_username"
+password = "your_password"
 
 # Define the login URL
 login_url = f"{apic_url}/api/aaaLogin.json"
